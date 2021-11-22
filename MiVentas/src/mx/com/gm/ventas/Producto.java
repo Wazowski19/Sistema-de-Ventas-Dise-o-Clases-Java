01757704fd7ae223110b5fab373645e1d2f031e6
Miguel <ma.garmar98@gmail.com>
2021-11-22 10:28:04 -0600

    first commit

---
+++ MiVentas/src/mx/com/gm/ventas/Producto.java
@@ -0,0 +1,52 @@
+
+package mx.com.gm.ventas;
+
+public class Producto {
+    private final int idProducto;
+    private String nombre;
+    private double precio;
+    private static int contadorProducto;
+    
+    private Producto(){
+        this.idProducto = ++Producto.contadorProducto;
+    }
+    
+    public Producto(String nombre, double precio){
+        this();
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getIdProducto() {
+        return this.idProducto;
+    }
+
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return this.precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Producto{idProducto=").append(this.idProducto);
+        sb.append(", nombre=").append(this.nombre);
+        sb.append(", precio=").append(this.precio);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+}
